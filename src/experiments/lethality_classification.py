06dee73dca38d4b3e5a0517d798b65868e9513fb
Alec Gower <61715970+AlecGower@users.noreply.github.com>
2023-06-03 00:43:59 +0200

    fixed f1 and jaccard score calculations

--- src/experiments/lethality_classification.py
+++ src/experiments/lethality_classification.py
@@ -25,19 +25,19 @@ ap.add_argument(
 )
 arguments = ap.parse_args()
 
-ess = pd.read_csv("data/sgd_ess.csv", index_col=0)
+ess = pd.read_csv("data/sgd_ess.csv", index_col=0).drop(columns='name')
 res = pd.read_csv(arguments.results_file, delimiter="\t").set_index("orf")
 
 if arguments.fba:
     print("Results:", len(res))
-    preds = res.join(ess).loc[:, ["inviable", "growth"]].fillna(0)
+    preds = res.join(ess, how='inner').loc[:, ["inviable", "growth"]].fillna(0)
     # # Changed because of train test split
     # preds = res.join(ess).loc[:, ["inviable", "growth"]].dropna()
     preds.growth = preds.growth > arguments.fba_threshold
     print("Preds:", len(preds))
 else:
     print("Results:", len(res))
-    preds = res.join(ess).loc[:, ["inviable", "growth"]].fillna(0)
+    preds = res.join(ess, how='inner').loc[:, ["inviable", "growth"]].fillna(0)
     # preds = res.join(ess).loc[:, ["inviable", "growth"]].dropna()
     print("Preds:", len(preds))
 
@@ -48,53 +48,53 @@ print("Majority class ('viable') classifier")
 
 print(classification_report(1 - majority.inviable, majority.growth, zero_division=0))
 
-cm = confusion_matrix(1 - majority.inviable, majority.growth)
+cm = confusion_matrix(majority.inviable, 1 - majority.growth)
 # print("Confusion matrix:\n\r", cm)
 print(
     pd.DataFrame(
         cm,
-        columns=pd.MultiIndex.from_product([["Predicted"], ["NG", "G"]]),
-        index=pd.MultiIndex.from_product([["Actual"], ["NG", "G"]]),
+        columns=pd.MultiIndex.from_product([["Predicted"], ["G", "NG"]]),
+        index=pd.MultiIndex.from_product([["Actual"], ["G", "NG"]]),
     ).to_markdown()
 )
 
-f1 = f1_score(1 - majority.inviable, majority.growth, zero_division=0)
+f1 = f1_score(majority.inviable, 1 - majority.growth, zero_division=0)
 print("\nF1 score:", f1)
 
-jaccard = jaccard_score(1 - majority.inviable, majority.growth,)
+jaccard = jaccard_score(majority.inviable, 1 - majority.growth,)
 print("Jaccard score: {:.3f}".format(jaccard))
 
 print("\nOur classifier")
 
 print(classification_report(1 - preds.inviable, preds.growth,))
 
-cm = confusion_matrix(1 - preds.inviable, preds.growth,)
+cm = confusion_matrix(preds.inviable, 1 - preds.growth,)
 # print("Confusion matrix:\n\r", cm)
 print(
     pd.DataFrame(
         cm,
-        columns=pd.MultiIndex.from_product([["Predicted"], ["NG", "G"]]),
-        index=pd.MultiIndex.from_product([["Actual"], ["NG", "G"]]),
+        columns=pd.MultiIndex.from_product([["Predicted"], ["G", "NG"]]),
+        index=pd.MultiIndex.from_product([["Actual"], ["G", "NG"]]),
     ).to_markdown()
 )
 
-f1 = f1_score(1 - preds.inviable, preds.growth,)
+f1 = f1_score(preds.inviable, 1 - preds.growth,)
 print("\nF1 score: {:.3f}".format(f1))
 
-jaccard = jaccard_score(1 - preds.inviable, preds.growth,)
+jaccard = jaccard_score(preds.inviable, 1 - preds.growth,)
 print("Jaccard score: {:.3f}".format(jaccard))
 
 ngg = preds[(preds.inviable == 0) & (preds.growth == 0)].join(
-    ess.loc[:, ["name", "full_name"]], how="left", rsuffix=".ess"
+    ess.loc[:, "full_name"], how="left", rsuffix=".ess"
 )
 ngng = preds[(preds.inviable == 1) & (preds.growth == 0)].join(
-    ess.loc[:, ["name", "full_name"]], how="left", rsuffix=".ess"
+    ess.loc[:, "full_name"], how="left", rsuffix=".ess"
 )
 gng = preds[(preds.inviable == 1) & (preds.growth == 1)].join(
-    ess.loc[:, ["name", "full_name"]], how="left", rsuffix=".ess"
+    ess.loc[:, "full_name"], how="left", rsuffix=".ess"
 )
 gg = preds[(preds.inviable == 0) & (preds.growth == 1)].join(
-    ess.loc[:, ["name", "full_name"]], how="left", rsuffix=".ess"
+    ess.loc[:, "full_name"], how="left", rsuffix=".ess"
 )
 
 
