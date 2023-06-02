94e092a428eb852c027b9adf19973f10bddc5f2e
Alec Gower <61715970+AlecGower@users.noreply.github.com>
2023-06-02 23:37:56 +0200

    logging for experiment 1 script

--- src/experiments/experiment1.sh
+++ src/experiments/experiment1.sh
@@ -1,8 +1,15 @@
-python3 experiments/create_theory_files.py "yeastGEM"
-python3 experiments/create_theory_files.py "iMM904"
-python3 experiments/create_theory_files.py "iND750"
+#!/bin/bash
+printf "Creating theory files for ... yeastGEM" &&
+	python3 experiments/create_theory_files.py "yeastGEM" &&
+	print "Done!"
+printf "Creating theory files for ... iMM904" &&
+	python3 experiments/create_theory_files.py "iMM904" &&
+	print "Done!"
+printf "Creating theory files for ... iND750" &&
+	python3 experiments/create_theory_files.py "iND750" &&
+	print "Done!"
 
 for model in $(ls experiments/theories/); do
 	python3 experiments/single_gene_deletions.py \
-        experiments/theories/$model/$(ls experiments/theories/$model | tail -n 1)
+		experiments/theories/$model/$(ls experiments/theories/$model | tail -n 1)
 done
