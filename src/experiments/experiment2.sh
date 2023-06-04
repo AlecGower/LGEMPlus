b5f866c9d058fd852930010c50cf8512c4cd51cb
Alec Gower <61715970+AlecGower@users.noreply.github.com>
2023-06-04 15:04:28 +0200

    experiment 2 script added

---
+++ src/experiments/experiment2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###
+# Experiment 2: Abduction of hypotheses
+# For each NGG error in the list:
+#   Run iProver and extract hypotheses to fix the error
+###
+
+for model in $(ls experiments/theories/); do
+	latest=$model/$(ls experiments/theories/$model | tail -n 1)
+	cut -w -f 1 experiments/results/$latest/log_ngg.txt |
+		xargs -n 1 -P 8 -I{} /bin/bash \
+			helpers/abduction/false_positive_abduction_preparation.sh \
+			{} \
+			experiments/theories/$latest/genes.p \
+			experiments/theories/$latest/reactions.p \
+			experiments/theories/$latest/media_compounds.p \
+			experiments/theories/$latest/ubiquitous_compounds.p \
+			experiments/theories/$latest/abduced_extra_compounds.p \
+			experiments/theories/$latest/query.p
+done
